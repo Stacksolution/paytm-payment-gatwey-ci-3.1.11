6ec0c2f332d74f45856abeaa63deab173fd768f5
Stack Solution <78157215+Stacksolution@users.noreply.github.com>
2021-10-14 11:52:03 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Welcome extends CI_Controller {
 
 	/**
@@ -18,8 +17,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	public function index(){
+		$this->load->view('paytm-check-out-page');
 	}
 }
